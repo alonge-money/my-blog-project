ef85e578974fa1ac3979b72077886a1e688b2e25
alonge-money <abdulazeezabibulai@gmail.com>
2024-07-25 14:19:25 +0100

    add build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 pip install -r requirements.txt
-python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic
-python -m gunicorn blogcoonfiguration.wsgi:application
+
 
 
